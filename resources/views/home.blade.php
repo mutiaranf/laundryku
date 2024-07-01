05a566524631cbefc434f37e922ea74b7d6eca90
Muhammad Ali Faatikh Riziq <faatikhriziq@gmail.com>
2024-07-01 10:33:59 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <title>laundryku - Clean & White </title>
@@ -557,7 +558,7 @@ https://templatemo.com/tm-586-scholar
       </div>
     </div>
   </footer>
-  
+
   @livewireScripts
   <!-- Scripts -->
   <!-- Bootstrap core JavaScript -->
